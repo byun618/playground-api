68b74c675586595668b21df8c1d93d0d91a08e52
byun618 <byun618@gmail.com>
2022-09-18 22:28:45 +0900

    auth 라우터 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
-import { PrismaModule } from './prisma/prisma.module'
 import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
